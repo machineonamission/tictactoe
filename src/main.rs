074321df3c506f5bb791d4106b3a970a192c5417
Hex <35177464+HexCodeFFF@users.noreply.github.com>
2023-10-21 01:15:43 -0500

    rus

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 #[derive(PartialEq)]
 #[derive(Clone)]
+#[derive(Default)]
 enum BoardSpace {
-    EMPTY,
+    #[default]
+    Empty,
     X,
     O,
 }
+
+// to string
 impl std::fmt::Display for BoardSpace {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", match self {
-            BoardSpace::EMPTY => "",
+        match self {
+            BoardSpace::Empty => " ",
             BoardSpace::O => "O",
             BoardSpace::X => "X"
-        })
+        }.fmt(f)
     }
 }
+
+
 // could I generate this dynamically? yes. Is it easier and faster to do it manually? also yes
+// list of positions in the board that, if theyre all matched, means someone won
 static WINS: [[usize; 3]; 8] = [
     [0, 1, 2],
     [3, 4, 5],
@@ -31,15 +38,26 @@ fn check_for_win(board: &[BoardSpace; 9]) -> &BoardSpace {
         // 3 spaces in a row are identical
         if board[win_case[0]] == board[win_case[1]]
             && board[win_case[0]] == board[win_case[2]]
-        // and not empty
-            && board[win_case[0]] != BoardSpace::EMPTY {
+            // and not empty
+            && board[win_case[0]] != BoardSpace::Empty {
             return &board[win_case[0]];
         }
     }
-    return &BoardSpace::EMPTY;
+    return &BoardSpace::Empty;
+}
+
+fn print_board([a, b, c, d, e, f, g, h, i]: &[BoardSpace; 9]) {
+    println!(
+        "{a}|{b}|{c}\n\
+        ------\n\
+        {d}|{e}|{f}\n\
+        ------\n\
+        {g}|{h}|{i}",
+    )
 }
 
 fn main() {
-    let board: [BoardSpace; 9];
+    let mut board: [BoardSpace; 9] = Default::default();
     println!("Hello, world!");
+    print_board(&board);
 }
